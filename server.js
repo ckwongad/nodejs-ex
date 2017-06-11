76a844c3ea5200602e2bf9fd1255b62a353fbc35
ckwongad <ckwongad@gmail.com>
2017-06-11 18:42:13 +0800

    - first test

--- server.js
+++ server.js
@@ -91,6 +91,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/test', function (req, res) {
+	res.send('test');
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
